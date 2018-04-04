43160545cc88189289d0be098c454fb86e6ba5a5
Flier Lu <flier.lu@gmail.com>
2018-04-04 17:35:37 +0800

    add test cases

--- src/parser/typeuse.rs
+++ src/parser/typeuse.rs
@@ -15,3 +15,46 @@ named!(
 );
 
 named!(use_type<Var>, delimited!(LPAR, preceded!(TYPE, typeidx), RPAR));
+
+#[cfg(test)]
+mod tests {
+    use std::str;
+
+    use nom::IResult::Done;
+    use parity_wasm::elements::ValueType::*;
+    use pretty_env_logger;
+
+    use super::*;
+
+    #[test]
+    fn parse_typedef() {
+        let _ = pretty_env_logger::try_init();
+
+        let tests: Vec<(&[u8], _)> = vec![
+            (b"(type $sig)", (Some(Var::Id("sig".to_owned())), None)),
+            (b"(type 0)", (Some(Var::Index(0)), None)),
+            (b"(param i64)", (None, Some(FunctionType::new(vec![I64], None)))),
+            (b"(result i64)", (None, Some(FunctionType::new(vec![], Some(I64))))),
+            (
+                b"(param i64) (result i64)",
+                (None, Some(FunctionType::new(vec![I64], Some(I64)))),
+            ),
+            (
+                b"(type $over-i64) (param i64) (result i64)",
+                (
+                    Some(Var::Id("over-i64".to_owned())),
+                    Some(FunctionType::new(vec![I64], Some(I64))),
+                ),
+            ),
+        ];
+
+        for (code, ref func_type) in tests {
+            assert_eq!(
+                typeuse(code),
+                Done(&[][..], func_type.clone()),
+                "parse typeuse: {}",
+                unsafe { str::from_utf8_unchecked(code) },
+            );
+        }
+    }
+}
